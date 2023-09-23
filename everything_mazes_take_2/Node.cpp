fee13810be9eec6ed808e7ad79b3240abfed5ded
Levi <levivanwingerden@hotmail.com>
2023-09-23 12:51:40 +0200

    added a node class.

---
+++ everything_mazes_take_2/Node.cpp
@@ -0,0 +1,116 @@
+#include "Header.h"
+
+Node::Node(int xPos, int yPos)
+{
+	position = std::pair<int, int>(std::make_pair(xPos, yPos));
+}
+
+int Node::getSide(std::shared_ptr<Node> neighbor)
+{
+	int side = -1;
+	if (position.first > neighbor->position.first)
+	{
+		side = East;
+	}
+	if (position.first < neighbor->position.first)
+	{
+		side = West;
+	}
+	if (position.second < neighbor->position.second)
+	{
+		side = North;
+	}
+	if (position.second > neighbor->position.second)
+	{
+		side = South;
+	}
+	return side;
+}
+
+
+
+std::shared_ptr<Node> Node::getNeighbor(int index) 
+{ 
+	return neighbors[index];
+}
+
+void Node::setNeighbor(int index, std::shared_ptr<Node> neighbor)
+{
+	neighbors[index] = neighbor;
+}
+
+MazeNode::MazeNode(int xPos, int yPos) : Node(xPos, yPos)
+{
+	position = std::pair<int, int>(std::make_pair(xPos, yPos));
+}
+int MazeNode::getSide(std::shared_ptr<MazeNode> neighbor)
+{
+	int side = -1;
+	if (position.first > neighbor->position.first)
+	{
+		side = East;
+	}
+	if (position.first < neighbor->position.first)
+	{
+		side = West;
+	}
+	if (position.second < neighbor->position.second)
+	{
+		side = North;
+	}
+	if (position.second > neighbor->position.second)
+	{
+		side = South;
+	}
+	return side;
+}
+
+std::shared_ptr<MazeNode> MazeNode::getNeighbor(int index)
+{	
+	return std::static_pointer_cast<MazeNode>(neighbors[index]);
+}
+
+void MazeNode::setNeighbor(int index, std::shared_ptr<Node> neighbor)
+{
+	neighbors[index] = neighbor;
+}
+
+
+
+AStarNode::AStarNode(int xPos, int yPos) : Node(xPos, yPos)
+{
+	position = std::pair<int, int>(std::make_pair(xPos, yPos));
+	visited = false;
+}
+
+int AStarNode::getSide(std::shared_ptr<AStarNode> neighbor)
+{
+	int side = -1;
+	if (position.first > neighbor->position.first)
+	{
+		side = East;
+	}
+	if (position.first < neighbor->position.first)
+	{
+		side = West;
+	}
+	if (position.second < neighbor->position.second)
+	{
+		side = North;
+	}
+	if (position.second > neighbor->position.second)
+	{
+		side = South;
+	}
+	return side;
+}
+
+std::shared_ptr<AStarNode> AStarNode::getNeighbor(int index)
+{
+	return neighbors[index];
+}
+
+void AStarNode::setNeighbor(int index, std::shared_ptr<AStarNode> neighbor)
+{
+	neighbors[index] = neighbor;
+}
---
+++ everything_mazes_take_2/Node.h
@@ -0,0 +1,51 @@
+#pragma once
+
+class Node
+{
+protected:
+	std::shared_ptr<Node> neighbors[4];
+public:
+
+	cordinates position;
+	bool visited = false;
+
+	Node(int xPos, int yPos);
+
+	int getSide(std::shared_ptr<Node> neighbor);
+
+	std::shared_ptr<Node> getNeighbor(int index);
+	void setNeighbor(int index, std::shared_ptr<Node> neighbor);
+
+};
+
+class MazeNode : public Node
+{
+public:
+ 	bool partOfMaze = false;
+	bool partOfFrontier = false;
+
+
+	MazeNode(int xPos, int yPos);
+	int getSide(std::shared_ptr<MazeNode> neighbor);
+	
+	std::shared_ptr<MazeNode> getNeighbor(int index);
+	void setNeighbor(int index, std::shared_ptr<Node> neighbor);
+};
+
+class AStarNode : public Node
+{
+public:
+	double gCost;				//distance to starnode.
+	double fCost;				//distance to finishnode.
+	double hCost;				//combinde value of gCost and fCost.
+	std::shared_ptr<AStarNode> parent;
+	std::shared_ptr<AStarNode> child;
+	std::shared_ptr<AStarNode> neighbors[4];
+
+	AStarNode(int xPos, int yPos);
+
+	int getSide(std::shared_ptr<AStarNode> neighbor);
+
+	std::shared_ptr<AStarNode> getNeighbor(int index);
+	void setNeighbor(int index, std::shared_ptr<AStarNode> neighbor);
+};
