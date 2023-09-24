b221bfeba5ebf61382379ae6d416f26d1fe10376
Levi <levivanwingerden@hotmail.com>
2023-09-24 11:08:23 +0200

    finished the astar algorithm

--- everything_mazes_take_2/PathFinding.cpp
+++ everything_mazes_take_2/PathFinding.cpp
@@ -2,10 +2,10 @@
 
 double distance(cordinates start, cordinates end)
 {
-	return double(sqrt((pow(end.first, 2) - pow(start.first, 2)) + (pow(end.second, 2) - pow(start.second, 2))));
+	return double(sqrt(abs(pow(end.first, 2) - pow(start.first, 2)) + abs(pow(end.second, 2) - pow(start.second, 2))));
 }
 
-std::vector<cordinates> pathSolvingAStar(Grid& grid)
+std::vector<cordinates> pathSolvingAStar(Grid& grid, std::vector<std::shared_ptr<Object>>& renderList)
 {
 	//Node *nodes = nullptr;
 
@@ -39,16 +39,17 @@ std::vector<cordinates> pathSolvingAStar(Grid& grid)
 			std::shared_ptr<AStarNode> NodePtr = std::make_shared<AStarNode>(x, y);
 			nodes[myCords]->parent = nullptr;
 			nodes[myCords]->gCost = INFINITY;
-			nodes[myCords]->fCost = INFINITY;
+			nodes[myCords]->hCost = INFINITY;
+
 		}
 	}
 	//
 	std::shared_ptr<AStarNode> startNode = nodes[grid.startAndEndCords.first];
 	std::shared_ptr<AStarNode> finishNode = nodes[grid.startAndEndCords.second];
+	startNode->gCost = 0.0f;	
+	
 
 	std::shared_ptr<AStarNode> currentNode;
-	startNode->gCost= 0.0f;
-	startNode->fCost = distance(startNode->position, finishNode->position);
 	currentNode = startNode;
 
 	std::list<std::shared_ptr<AStarNode>> listNotTestedNodes;
@@ -95,7 +96,7 @@ std::vector<cordinates> pathSolvingAStar(Grid& grid)
 	//
 	while (!listNotTestedNodes.empty() && currentNode != finishNode)
 	{
-		listNotTestedNodes.sort([](const std::shared_ptr<AStarNode> lhs, const std::shared_ptr<AStarNode> rhs) {return lhs->hCost < rhs->hCost; });
+		listNotTestedNodes.sort([](const std::shared_ptr<AStarNode> lhs, const std::shared_ptr<AStarNode> rhs) {return lhs->fCost < rhs->fCost; });
 		while (!listNotTestedNodes.empty() && listNotTestedNodes.front()->visited)
 		{
 			listNotTestedNodes.pop_front();
@@ -116,9 +117,9 @@ std::vector<cordinates> pathSolvingAStar(Grid& grid)
 				}
 
 
-				float possiblyLowerGoal = currentNode->gCost + distance(currentNode->position, nodeNeightbour->position);
+				float possiblyLowerGoal = float(currentNode->gCost + distance(currentNode->position, nodeNeightbour->position));
 
-				if (possiblyLowerGoal < nodeNeightbour->gCost)
+				if (possiblyLowerGoal < nodeNeightbour->gCost && !grid.wallMap[currentNode->position][currentNode->getSide(nodeNeightbour)]->active)
 				{
 					nodeNeightbour->parent = currentNode;
 					nodeNeightbour->gCost = possiblyLowerGoal;
@@ -133,10 +134,19 @@ std::vector<cordinates> pathSolvingAStar(Grid& grid)
 	while (p->parent != nullptr)
 	{
 
-		grid.tileMap[p->position]->color = sf::Color::Black;
+		//grid.tileMap[p->position]->color = sf::Color::Black;
+		//grid.tileMap[p->position]->shape.setFillColor(grid.tileMap[p->position]->color);
+		float position1X = float((grid.plane->position.x - (grid.plane->size.x / 2) + ((p->position.first * (grid.tileSize)) + grid.tileSize / 2)));
+		float position1Y = float((grid.plane->position.y - (grid.plane->size.y / 2) + ((p->position.second * (grid.tileSize) + grid.tileSize / 2))));
+		float position2X = float((grid.plane->position.x - (grid.plane->size.x / 2) + ((p->parent->position.first * (grid.tileSize)) + grid.tileSize / 2)));
+		float position2Y = float((grid.plane->position.y - (grid.plane->size.y / 2) + ((p->parent->position.second * (grid.tileSize) + grid.tileSize / 2))));
+		std::shared_ptr<Line> line = std::make_shared<Line>(sf::Vector2f(position1X, position1Y), sf::Vector2f(position2X, position2Y));
+		renderList.emplace_back(line);
 		path.emplace_back(p->position);
+		std::cout << p->position.first << ", " << p->position.second << std::endl;
 		p = p->parent;
 	}
+	
 	
 	//return the solved path
 
--- everything_mazes_take_2/PathFinding.h
+++ everything_mazes_take_2/PathFinding.h
@@ -3,4 +3,4 @@
 
 double distance(cordinates start, cordinates end);
 
-std::vector<cordinates> pathSolvingAStar(Grid& grid);
+std::vector<cordinates> pathSolvingAStar(Grid& grid, std::vector<std::shared_ptr<Object>>& renderList);
