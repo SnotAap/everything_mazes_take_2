4b8bfe583c214954b6b998535a4301d592016769
Levi <levivanwingerden@hotmail.com>
2023-11-20 21:37:15 +0100

    updated the UI and integrated it in the main loop

--- everything_mazes_take_2/everything mazes.cpp
+++ everything_mazes_take_2/everything mazes.cpp
@@ -1,165 +1,127 @@
 #include "Header.h"
 
 std::vector<std::shared_ptr<Object>> renderList;
-std::vector<cordinates> expressedwallMap;
-
-//test
-
-//std::map<cordinates, int> floodedMaze;
-//int depth = -1;
-//std::vector<std::shared_ptr<Node>> toBeRemoved;
-//std::vector<std::shared_ptr<Node>> toBeAdded;
-//std::list<std::shared_ptr<Node>> queue;
-//std::map<cordinates, std::shared_ptr<Node>> nodes;
-
-//
-
-std::vector<cordinates> path;
-int sizeX = 20;
-int sizeY = 20;
+std::vector<std::pair<std::shared_ptr<Robot>, bool>> robotList;
 
 bool solve = false;
-
-#define generateState 0
-#define solveState 1
-#define solvedState 2
-#define filledState 3
 int state = 0;
+int sizeX = 20;
+int sizeY = 20;
 
 microTime currentTime;
 microTime lastTime;
 microTime deltaTime;
+
 sf::Mouse myMouse;
 sf::Font font;
 
-
 int main()
-{
+{    
     QueryPerformanceFrequency(&frequency);
-    lastTime = getMicroTime();
-    srand((unsigned int)getMicroTime());
-    UI myUI(renderList);
+    srand((unsigned int)getMicroTime());    
     Grid grid(sizeX, sizeY);
-    //grid.setup(renderList);
-
-
+    
+    UI myUI(grid, font, renderList);
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Mazes!");
-
-    //grid.recursiveBacktrackingMaze();   
-    //grid.primsMaze();   
-    
-    std::shared_ptr<Robot> robot0;
-    std::shared_ptr<Robot0> robot1;
-    
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
-        {            
-            myUI.checkButtonsPressed(myMouse, event);         
+        {   
+                    
             if (event.type == sf::Event::Closed)
                 window.close();
-
         }        
         window.clear(sf::Color::White);
         //
         currentTime = getMicroTime();
         deltaTime = currentTime - lastTime;
-        myUI.checkButtonsHoverd(myMouse);   
-
-        if (myUI.buttons[0]->IsPressed == true) 
-        {    
-            grid.resetGrid();
-            grid.setup(renderList);              
-            myUI.buttons[0]->IsPressed = false;
-        }
-        if (myUI.buttons[1]->IsPressed == true)
-        {             
-            grid.resetGrid();
-            grid.recursiveBacktrackingMaze();
-            grid.devRemoveWalls(); 
-            grid.removedWallsOrderd.clear();
-            myUI.buttons[1]->IsPressed = false;
-        }
-        if (myUI.buttons[2]->IsPressed == true)
-        {       
-            grid.resetGrid();
-            grid.primsMaze();
-            grid.devRemoveWalls();
-            grid.removedWallsOrderd.clear();
-            myUI.buttons[2]->IsPressed = false;            
-        } 
-        if (myUI.buttons[3]->IsPressed == true && solve == false)
-        {
-            robot0 = std::make_shared<Robot>(grid);
-            renderList.emplace_back(robot0);
-            robot1 = std::make_shared<Robot0>(grid);
-            renderList.emplace_back(robot1);
-            solve = true;            
-        }
-       
+        myUI.mousePressed(event);
+        myUI.checkButtonsPressed(myMouse);
+        myUI.checkButtonsHoverd(myMouse);  
+              
+        int action = myUI.checkButtonsPressed(myMouse);
+        myUI.buttonAction(font, state, action, grid, renderList, robotList, solve);
+        //myUI.ButtonManager(state);
         //
         switch (state)
         {
+        case startState:
+            myUI.activeButtons.clear();
+            myUI.activeButtons.emplace_back(myUI.buttons[0]);
+            myUI.activeButtons.emplace_back(myUI.buttons[1]);
+            myUI.activeButtons.emplace_back(myUI.buttons[2]);
+            myUI.activeButtons.emplace_back(myUI.buttons[3]);
+            myUI.activeButtons.emplace_back(myUI.buttons[4]);
+            myUI.activeButtons.emplace_back(myUI.buttons[5]);
+            myUI.activeButtons.emplace_back(myUI.buttons[6]);
+            myUI.activeButtons.emplace_back(myUI.buttons[7]);
+            myUI.activeButtons.emplace_back(myUI.buttons[8]);
+            myUI.activeButtons.emplace_back(myUI.buttons[9]);
+            myUI.activeButtons.emplace_back(myUI.buttons[10]);
+
+            break;
+        case generateChoiseState:
+            myUI.activeButtons.clear();
+            myUI.activeButtons.emplace_back(myUI.buttons[11]);
+            myUI.activeButtons.emplace_back(myUI.buttons[12]);
+
+            break;
         case generateState:
-            
-            //if (grid.removeWalls(deltaTime))
-            //{        
-            //    nodes = setupNodes(grid, floodedMaze, queue);
-            //    state++;
-            //} 
-            if (grid.devRemoveWalls())
+            myUI.activeButtons.clear();
+            myUI.activeButtons.emplace_back(myUI.buttons[13]);
+
+            if (grid.removeWalls(deltaTime))
             {
-                //nodes = setupNodes(grid, floodedMaze, queue);
                 state++;
             }
- 
-            
             break;
         case solveState:
-            
-            if (solve == true) 
+            myUI.activeButtons.clear();
+            myUI.activeButtons.emplace_back(myUI.buttons[14]);
+            myUI.activeButtons.emplace_back(myUI.buttons[15]);
+            myUI.activeButtons.emplace_back(myUI.buttons[16]);
+            myUI.activeButtons.emplace_back(myUI.buttons[17]);
+
+            break;
+        case solvingState:
+            myUI.activeButtons.clear();
+            myUI.activeButtons.emplace_back(myUI.buttons[18]);
+
+            for (int i = 0; i < robotList.size(); i++)
             {
-                if (robot0->gridPos != grid.startAndEndCords.second)
+                if (robotList[i].second)
                 {
-                    robot0->movement(grid, deltaTime);
+                    robotList[i].first->movement(grid, deltaTime, renderList);                                            
                 }
-                if (robot1->gridPos != grid.startAndEndCords.second)
-                {
-                    robot1->movement(grid, deltaTime);                    
-                }
-                else
-                {
-                    pathSolvingAStar(grid, renderList);
-                    state++;
-                } 
-                //if (robot1->gridPos == grid.startAndEndCords.second) 
-                //{
-                //    solve = false;
-                //    myUI.buttons[3]->IsPressed = false;
-                //}
-            }           
+            }
             break;
-        case solvedState:
-            
-            
-            break;
-
-
         }
-        //
         for (int i = 0; i < renderList.size(); i++)
         {            
             renderList[i]->draw(&window);             
         }
-        //for (int i = 0; i < grid.depthList.size(); i++)
-        //{
-        //    grid.depthList[i]->draw(&window);
-        //}
-        //
+        
+        for (int i = 0; i < myUI.activeButtons.size(); i++)
+        {
+            myUI.activeButtons[i]->draw(&window);
+        }
+        for (int i = 0; i < robotList.size(); i++)
+        {
+            if (robotList[i].second)
+            {
+                robotList[i].first->draw(&window);
+            }
+        }
+        for (int i = 0; i < grid.depthList.size(); i++)
+        {
+            grid.depthList[i]->draw(&window);
+        }
+        
         window.display();
         lastTime = currentTime;
+        myUI.lastLeftMousePressed = myUI.LeftMousePressed;
     }
     return 0;
 }
